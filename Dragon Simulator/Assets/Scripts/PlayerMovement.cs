9889cf5c25e3a39806ed091f920630d644378b9e
knevin123 <c14447012@mydit.ie>
2018-01-27 17:26:10 +0000

    fixed bugs with random spikes in terrain and undeleting terrain

---
+++ Dragon Simulator/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	public float rotSpeed=40.0f;
+	public float moveSpeed=10.0f;
+
+	void Update()
+	{
+
+
+		if (Input.GetKey(KeyCode.W))
+		{
+			transform.Translate(0, 0, moveSpeed * Time.deltaTime);
+		}
+		if (Input.GetKey(KeyCode.A))
+		{
+			transform.Rotate(Vector3.up,rotSpeed*Time.deltaTime);
+		}
+	}
+}
+
