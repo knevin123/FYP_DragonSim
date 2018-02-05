28e2d8af0e6215bf7c612208c09653a42be8a3fb
knevin123 <c14447012@mydit.ie>
2018-02-05 14:03:22 +0000

    added dragon model,new day night cycle

---
+++ Dragon Simulator/Assets/Scripts/CameraController.cs
@@ -0,0 +1,71 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	public GameObject dragon;
+
+
+	float speed = 200.0f;
+	// Use this for initialization
+	void Start()
+	{
+	}
+
+	void Yaw(float angle)
+	{
+		Quaternion rot = Quaternion.AngleAxis(angle, Vector3.up);
+		transform.rotation = rot * transform.rotation;
+	}
+
+	void Roll(float angle)
+	{
+		Quaternion rot = Quaternion.AngleAxis(angle, Vector3.forward);
+		transform.rotation = rot * transform.rotation;
+	}
+
+	void Pitch(float angle)
+	{
+		float invcosTheta1 = Vector3.Dot(transform.forward, Vector3.up);
+		float threshold = 0.95f;
+		if ((angle > 0 && invcosTheta1 < (-threshold)) || (angle < 0 && invcosTheta1 > (threshold)))
+		{
+			return;
+		}
+
+		// A pitch is a rotation around the right vector
+		Quaternion rot = Quaternion.AngleAxis(angle, transform.right);
+
+		transform.rotation = rot * transform.rotation;
+	}
+
+	void Walk(float units)
+	{
+		transform.position += transform.forward * units;
+	}
+
+	void Fly(float units)
+	{
+		transform.position += Vector3.up * units;
+	}
+
+	void Strafe(float units)
+	{
+		transform.position += transform.right * units;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float mouseX, mouseY;
+		float speed = this.speed;
+		mouseX = Input.GetAxis("Mouse X");
+		mouseY = Input.GetAxis("Mouse Y");
+		Yaw(mouseX);
+		Pitch(-mouseY);
+
+		//change position to above the dragon
+		transform.position = dragon.transform.position;
+	}
+
+}
---
+++ Dragon Simulator/Assets/Scripts/DragonMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragonMove : MonoBehaviour {
+
+	public int forwardspeed=2;
+	public int rotatespeed=3;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			transform.Rotate(Vector3.left*rotatespeed);
+		}
+		if (Input.GetKey(KeyCode.DownArrow))
+		{
+			transform.Rotate(Vector3.right*rotatespeed);
+		}
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.Rotate(Vector3.down*rotatespeed);
+		}
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.Rotate(Vector3.up*rotatespeed);
+		}
+		transform.Translate(0, 0,  forwardspeed* Time.deltaTime);	
+	}
+}
